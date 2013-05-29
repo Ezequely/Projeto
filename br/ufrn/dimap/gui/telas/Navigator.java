6241172f718bfcb2b835597b1fd04d3f9ccef6f7
souzabrizolara <souzabrizolara@gmail.com>
2013-05-29 11:33:46 -0300

    Add interface Navigator

---
+++ br/ufrn/dimap/gui/telas/Navigator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufrn.dimap.gui.telas;
+
+/**
+ *
+ * @author leobrizolara
+ */
+public interface Navigator {
+    public void setNavigableParent(Navigable parent);
+    public Navigable getNavigableParent();
+}
